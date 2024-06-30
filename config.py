ed23d5ce4bc4b6c751a4942d85ba0258fb6d53e8
Kim Kihyeon <kihyeon731@gmail.com>
2024-07-01 08:49:28 +0900

    commit2

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ file_name3 = "3_약국기본정보_좌표_전화번호_240626.csv"
 file_name4 = "4_약국기본정보_좌표_보강_240626.csv"
 file_name5 = "5_약국기본정보_전화번호_보강240626.csv"
 
-pharmacy_key = 'x4N+cgBxTUXXIVNkNmkDLAPQtVutbbVH7QQNMQ2Tm8cmGkX69RyzQPRNk2Rpd7/YinVJnK/iYPlOsyKNk6+VmQ=='
+pharmacy_key = 'your_api_key'
 
-kakao_api = '3157d18317086de1c03c8a52520946d6'
+kakao_api = 'your_api_key'
 
-naver_client_id = 'hHRfNfl2HpkT9Nu09e1j'
-naver_client_secret = 'VkAxC8XR8a'
+naver_client_id = 'your_api_key'
+naver_client_secret = 'your_api_key'
