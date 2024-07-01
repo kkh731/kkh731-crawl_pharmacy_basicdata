1826f05127281565eabc32773531f49316735478
Kim Kihyeon <kihyeon731@gmail.com>
2024-07-01 09:02:16 +0900

    gitignore test

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ file_name3 = "3_약국기본정보_좌표_전화번호_240626.csv"
 file_name4 = "4_약국기본정보_좌표_보강_240626.csv"
 file_name5 = "5_약국기본정보_전화번호_보강240626.csv"
 
-pharmacy_key = 'your_api_key'
+pharmacy_key = 'your_api_key12'
 
 kakao_api = 'your_api_key'
 
